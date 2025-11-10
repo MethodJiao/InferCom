3374507e930a4f8856bdba65c16d183e83a696fa
ZhaoNinghui <2875544698@qq.com>
2025-11-10 16:52:18 +0800

    新的脚本

---
+++ read_repo.py
@@ -0,0 +1,26 @@
+import os
+from build_cpp_func_base import FuncBaseBuilder as CppFuncBaseBuilder
+from utils.utils import Utils
+
+@staticmethod
+def read_repo(repo_dir):
+    entries = os.listdir(repo_dir)
+    repos = [entry for entry in entries if os.path.isdir(os.path.join(repo_dir, entry))]
+    func_base_builder = CppFuncBaseBuilder(repos, repo_dir)
+    func_base_builder.build(benchmark='test')
+    # 设置需要读取的文件路径
+    temp2 = Utils.load_pickle('cache/func_base/test_C++Examples.pkl')
+    # 设置输出文件路径
+    outpath = 'cpp_func_test.txt'
+    with open(outpath, 'w', encoding='utf-8') as outfile:
+        for func in temp2:
+            # 将每个方法写入txt文件中，方法之间用****分隔
+            outfile.write(func['metadata']['func_body'] + '\n****\n')
+
+if __name__ == '__main__':
+    # 设置需要读取的文件路径
+    repo_dir = 'repos/sota_test/C++Examples'  # Replace with your actual repo directory
+    read_repo(repo_dir)
+
+
+
